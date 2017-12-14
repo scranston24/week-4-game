a91fa7717cc7f868f9fb20c3af51a4c72699e3ce
Sean Cranston <scranston24@Outlook.com>
2017-12-13 19:40:49 -0500

    added

---
+++ assets/javascript/game.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+  var randomNumber=Math.floor(Math.random()*101+19)
+
+  $('#randomNumber').text(randomNumber);
+
+  var num1= Math.floor(Math.random()*11+1)
+  var num2= Math.floor(Math.random()*11+1)
+  var num3= Math.floor(Math.random()*11+1)
+  var num4= Math.floor(Math.random()*11+1)
+
+  var total= 0;
+  var wins= 0;
+  var losses= 0;
+
+  $('#numberWins').text(wins);
+  $('#numberLosses').text(losses);
+
+  function reset(){
+    randomNumber=Math.floor(Math.random()*101+19);
+    $('#randomNumber').text(randomNumber);
+    num1= Math.floor(Math.random()*11+1);
+    num2= Math.floor(Math.random()*11+1);
+    num3= Math.floor(Math.random()*11+1);
+    num4= Math.floor(Math.random()*11+1);
+    userTotal= 0;
+    $('#finalTotal').text(total);
+  }
+
+  function winner(){
+    alert("You Won the 2017 NBA FINALS!");
+      wins++;
+      $('#numberWins').text(wins);
+      reset();
+  }
+
+  function loser(){
+    alert("Better Luck Next Year...");
+      losses++;
+      $('#numberLosses').text(losses);
+      reset();
+}
+
+  $('#wall').on('click', function(){
+    total = total + num1;
+    $('#finalTotal').text(total);
+
+      if(total == randomNumber){
+        winner();
+      }
+      else if (total > randomNumber){
+        loser();
+      }
+  })
+  $('#james').on('click', function(){
+    total = total + num2;
+    $('#finalTotal').text(total);
+
+      if(total == randomNumber){
+        winner();
+      }
+      else if (total > randomNumber){
+        loser();
+      }
+  })
+  $('#curry').on('click', function(){
+    total = total + num3;
+    $('#finalTotal').text(total);
+
+      if(total == randomNumber){
+        winner();
+      }
+      else if (total > randomNumber){
+        loser();
+      }
+  })
+  $('#westbrook').on('click', function(){
+    total = total + num4;
+    $('#finalTotal').text(total);
+
+      if(total == randomNumber){
+        winner();
+      }
+      else if (total > randomNumber){
+        loser();
+      }
+  });
+});
